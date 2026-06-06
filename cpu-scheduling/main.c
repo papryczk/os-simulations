83a748a31a4325ddc2220bcbaf001143c8d97adf
Papryczka <ppapryczka@protonmail.com>
2026-06-06 20:54:53 +0000

    added flags parsing; prepared for choosing algorithms

--- cpu-scheduling/main.c
+++ cpu-scheduling/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define BUFFER_SIZE 128
 #define INITIAL_CAPACITY 8
@@ -7,15 +8,16 @@
 typedef struct {
 	int arrivalTime;
 	int duration;
+	int isDone;
 } Process;
 
-Process* parseInput(int* outCount) {
+Process *parseInput(int *outCount) {
 	char buffer[BUFFER_SIZE];
 	int processCount = 0;
 	int capacity = INITIAL_CAPACITY;
 	int lineCount = 0;
 
-	Process* processes = malloc(capacity * sizeof(Process));
+	Process *processes = malloc(capacity * sizeof(Process));
 	if (processes == NULL) {
 		fprintf(stderr, "Memory allocation failed!\n");
 		return NULL;
@@ -26,7 +28,7 @@ Process* parseInput(int* outCount) {
 		if (buffer[0] == '\n') continue;
 		if (processCount >= capacity) {
 			capacity *= 2;
-			Process* tmp = realloc(processes, capacity*sizeof(Process));
+			Process *tmp = realloc(processes, capacity*sizeof(Process));
 			if (tmp == NULL) {
 				fprintf(stderr, "Memory allocation failed!\n");
 				free(processes);
@@ -35,6 +37,7 @@ Process* parseInput(int* outCount) {
 			processes = tmp;
 		}
 		if(sscanf(buffer, "%d %d", &processes[processCount].arrivalTime, &processes[processCount].duration)==2) {
+			processes[processCount].isDone = 0;
 			processCount++;
 		} else {
 			fprintf(stderr, "Incorrect line format in line %d\n", lineCount);
@@ -48,13 +51,47 @@ Process* parseInput(int* outCount) {
 
 }
 
-int main(int argc, char* argv[]) {
+int comparProcesses(const void *a, const void *b) {
+	const Process *processA = (const Process*)a;
+	const Process *processB = (const Process*)b;
+
+	return (processA->arrivalTime - processB->arrivalTime);
+}
+
+int main(int argc, char *argv[]) {
+	char algorithm = '\x00';
+	int opt;
+	while ((opt = getopt(argc, argv, "hfl")) != -1) {
+		switch (opt) {
+			case 'f': algorithm = 'f'; break;
+			case 'l': algorithm = 'l'; break;
+			case 'h':	// fall-through
+			case '?':
+				printf("HELP\n");	// TODO
+				return 0;
+
+		}
+	}
+	if (algorithm == '\x00') {
+    	fprintf(stderr, "No algorithm chosen! Use -h flag for help!");
+    	return 1;
+	}
+
 	int processCount = 0;
-	Process* processes = parseInput(&processCount);
+	Process *processes = parseInput(&processCount);
 	if(processes == NULL) return 1;
+	
+	qsort(processes, processCount, sizeof(Process), comparProcesses);
 
+	switch (algorithm) {
+		case 'f':
+			//algoFCFS();	// TODO
+			break;
+		case 'l':
+			//algoLCSF();	// TODO
+			break;
 
-
+	}
 	free(processes);
 	return 0;
 }
